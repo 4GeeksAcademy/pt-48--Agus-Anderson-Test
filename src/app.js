b99459e9b778d41c72b7dffd2b31acabf30eae71
Agustín Ruiz <143494692+GoldenDrk@users.noreply.github.com>
2023-10-19 23:05:30 +0000

    Página funcional

--- src/app.js
+++ src/app.js
@@ -5,7 +5,34 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function obtenerPromedio() {
+  let reaction = document.querySelector("#reaction").value;
+  let memory = document.querySelector("#memory").value;
+  let verbal = document.querySelector("#verbal").value;
+  let visual = document.querySelector("#visual").value;
+
+  let total = [
+    parseInt(reaction),
+    parseInt(memory),
+    parseInt(verbal),
+    parseInt(visual)
+  ];
+
+  const sumTotal = total.reduce(
+    (accumulator, currentValue) => accumulator + currentValue
+  );
+
+  let promedio = parseInt(sumTotal) / 4;
+
+  document.querySelector("#promedio").innerHTML = promedio;
+
+  function mostrarMensaje() {
+    promedio >= 75
+      ? (document.querySelector("#mensaje").innerHTML =
+          "Great You scored higher than 65% of the people to have taken these test.")
+      : (document.querySelector("#mensaje").innerHTML = "Trabaja mejor");
+  }
+  mostrarMensaje();
+}
+
+window.obtenerPromedio = obtenerPromedio;
